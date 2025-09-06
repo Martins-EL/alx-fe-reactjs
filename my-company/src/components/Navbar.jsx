6c1ed2f47e1d140dbfe627f94deef52b4dd192c9
Martins Liyansan <liyansanm@gmail.com>
2025-09-06 23:34:33 +0100

    updated commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{display:'flex', flexDirection:'row', color:'yellow'}}>
+    <nav style={{display:'flex', flexDirection:'row', color:'yellow', backgroundColor:'Blue' , justifyContent:'space-between'}}>
       <ul>
         <li><Link to="/ Home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
