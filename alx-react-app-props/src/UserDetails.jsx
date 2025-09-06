d4d0d171a7a416bd8dcff52bd0c43bd6fb094b03
Martins Liyansan <liyansanm@gmail.com>
2025-09-06 13:57:30 +0100

    corrected commit

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import UserContext from "./utils/UserContext";
+import UserContext from "./UserContext";
 function UserDetails() {
     const data=useContext(UserContext)
   return (
