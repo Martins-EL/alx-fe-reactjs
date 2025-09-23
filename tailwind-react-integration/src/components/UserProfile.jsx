4fff7bb0f5ea80c9072550130ee814e557d325fe
Martins Liyansan <liyansanm@gmail.com>
2025-09-23 18:03:36 +0100

    corrected commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm np mx-auto, my-20 rounded-lg shadow-lg md:p-8 sm:p-4 max-w-xs md:max-w-sm  sm:text-sm  md:text-base sm:w-24 h-24 md:w-36 h-36 sm:text-lg md:text-xl">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm np mx-auto, my-20 rounded-lg shadow-lg md:p-8 sm:p-4 max-w-xs md:max-w-sm  sm:text-sm  md:text-base sm:w-24 h-24 md:w-36 h-36 sm:text-lg md:text-xl sm:h-24 md:h-36">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
