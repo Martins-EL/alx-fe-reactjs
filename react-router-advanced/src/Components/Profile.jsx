16d30cbc973ee9a56cc4b3fd6deab503560de9e6
Martins Liyansan <liyansanm@gmail.com>
2025-10-19 20:44:52 +0100

    updated commit

--- react-router-advanced/src/Components/Profile.jsx
+++ react-router-advanced/src/Components/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import ProfileDetails from "./ProfileDetails";
-import ProfileSettings from "./ProfileSettings";
+import ProfileDetails from "../ProfileDetails";
+import ProfileSettings from "../ProfileSettings";
 
 function Profile() {
   return (
